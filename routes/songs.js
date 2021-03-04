b2804705c7f584f46894360d7b193a3cb0844dce
fmmyriam <benhmyriam@gmail.com>
2021-03-04 14:48:37 +0100

    created pages and create song form, started building songmodel

--- routes/songs.js
+++ routes/songs.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET songs page. */
 router.get('/my-songs', (req, res, next) => {
-  res.render('my-songs.hbs', { title: 'Express' });
+  res.render('./../views/dashboard/my-songs.hbs');
 });
+
+// GET create song page
+router.get('/my-songs/create', (req, res, next) => {
+    res.render('./../views/dashboard/create-song.hbs');
+  });
+
+// POST create song page
+router.post('/my-songs/create', (req, res, next) => {
+    console.log('song successfully created :D')
+    res.render('./../views/dashboard/my-songs.hbs');
+  });
 
 
 module.exports = router;
